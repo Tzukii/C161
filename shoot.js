e3bacd7887517c2963211119a5ca59b7da7b77fa
Shivam Maji <70868901+ParadoxFlame@users.noreply.github.com>
2022-12-05 21:05:32 -0800

    C162

--- shoot.js
+++ shoot.js
@@ -33,8 +33,44 @@ AFRAME.registerComponent("bullets", {
 
                 var scene = document.querySelector("#scene");
 
+                bullet.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: "0",
+                });
+
+                bullet.addEventListener("collide", this.removeBullet);
+
                 scene.appendChild(bullet);
             }
         });
+    },
+
+    removeBullet: function (e) {
+        console.log(e.detail.target.el);
+
+        console.log(e.detail.body.el);
+
+        var element = e.detail.target.el;
+
+        var elementHit = e.detail.body.el;
+
+        if (elementHit.id.includes("box")) {
+            elementHit.setAttribute("material", {
+                opacity: 1,
+                transparent: true,
+            });
+
+            var impulse = new CANNON.Vec3(-2, 2, 1);
+            var worldPoint = new CANNON.Vec3().copy(
+                elementHit.getAttribute("position")
+            );
+
+            elementHit.body.applyImpulse(impulse, worldPoint);
+
+            element.removeEventListener("collide", this.shoot);
+
+            var scene = document.querySelector("#scene");
+            scene.removeChild(element);
+        }
     },
 });
