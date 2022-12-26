7617273c5ccb4e3cf0a3731dd4e3e457b37cfe15
Shivam Maji <70868901+ParadoxFlame@users.noreply.github.com>
2022-12-26 08:43:08 -0800

    12/26 Box Spawns

--- gameObjects.js
+++ gameObjects.js
@@ -89,13 +89,26 @@ AFRAME.registerComponent("boxes", {
         depth: { type: "number", default: 3 },
     },
     init: function () {
+
+        //x position array
+        px = [22.86, -17.35, -12.81, 0.44, -30.18,
+            -25.89, 15.61, 29.68, 11.95, -15.40,
+            -14.09, 34.76, 2.29, 21.77, 1.57,
+            34.72, 12.04, -10.90, 6.48, 15.66];
+
+        //z position array
+        pz = [54.56, -4.71, 14.91, 56.74, 41.13,
+            50.76, 57.84, 7.02, -5.24, -26.82,
+            27.59, -35.78, 34.52, 31.32, -9.22,
+            26.72, 48.90, 27.24, 9.94, 54.29];
+
         for (var i = 0; i < 20; i++) {
             var box = document.createElement("a-entity");
             box.setAttribute("id", "box" + i);
 
-            posX = Math.random() * 200 - 100;
-            posY = 1.5;
-            posZ = Math.random() * 200 - 100;
+            posX = px[i];
+            posY = 1;
+            posZ = pz[i];
 
             position = { x: posX, y: posY, z: posZ };
             box.setAttribute("position", position);
